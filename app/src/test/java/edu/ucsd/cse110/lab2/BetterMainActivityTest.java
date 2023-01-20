3f12b4a2a83ae957e240ff22ba08e974a97663c0
akhil841 <akhilschoolemail@gmail.com>
2023-01-19 19:21:27 -0800

    added new tests

---
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -0,0 +1,35 @@
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest {
+    @Test
+    public void test_one_plus_one_equals_two()
+    {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+           Button oneBut = activity.findViewById(R.id.btn_one);
+           Button plusBut = activity.findViewById(R.id.btn_plus);
+           Button eqBut = activity.findViewById(R.id.btn_equals);
+           oneBut.performClick();
+           plusBut.performClick();
+           oneBut.performClick();
+           eqBut.performClick();
+           TextView display = activity.findViewById(R.id.display);
+           assertEquals("2", display.getText());
+        });
+    }
+}
